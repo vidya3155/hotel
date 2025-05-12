415a21a35998609fbfd665184399e849e5ca83ad
vidya3155 <vidymns3155@gmail.com>
2025-05-12 22:18:28 +0700

    binding database

--- api/index.ts
+++ api/index.ts
@@ -1,32 +1,15 @@
 interface Env {
     ASSETS: Fetcher;
+    DB: D1Database;
   }
   
   export default {
-    fetch(request, env) {
+    async fetch(request, env) {
       const url = new URL(request.url);
   
       if (url.pathname.startsWith("/api/events")) {
-        return Response.json([
-          {
-            id: '1',
-            name: "event 1",
-            place: "pandaan",
-            time: 1741247092,
-          },
-          {
-            id: '2',
-            name: "event 2",
-            place: "sukorejo",
-            time: 1741246092,
-          },
-          {
-            id: '3',
-            name: "event 3",
-            place: "purwosari",
-            time: 1741245092,
-          },
-        ]);
+        let { results } = await env.DB.prepare("SELECT * FROM events").all();
+        return Response.json(results);
       }
   
       return env.ASSETS.fetch(request);
