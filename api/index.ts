04ede6ec9ee56a5039fdbf78b2cf4876a35b64eb
vidya3155 <vidymns3155@gmail.com>
2025-05-22 15:15:00 +0700

    update dan delete data

--- api/index.ts
+++ api/index.ts
@@ -7,17 +7,42 @@ type Bindings = {
   
 const app = new Hono<{ Bindings: Bindings }>()
 
-app.get('/api/events', async (c) => {
-  let { results } = await c.env.DB.prepare("SELECT * FROM events").all()
+app.get('/api/hotels', async (c) => {
+  let { results } = await c.env.DB.prepare("SELECT * FROM hotels").all()
   return c.json(results)
 })
   
-app.post('/api/events', async (c) => {
+app.post('/api/hotels', async (c) => {
   const newId = crypto.randomUUID()
   const input = await c.req.json<any>()
-  const query = `INSERT INTO events(id,name,place,time) values ("${newId}","${input.name}","${input.place}",${input.time})`
-  const newEvent = await c.env.DB.exec(query)
-  return c.json(newEvent)
+  const query = `INSERT INTO hotels(id,name,place,time) values ("${newId}","${input.name}","${input.place}",${input.time})`
+  const newHotel = await c.env.DB.exec(query)
+  return c.json(newHotel)
 })
   
+app.get('/api/hotels/:id', async (c) => {
+  const hotelId = c.req.param('id')
+  let { results } = await c.env.DB.prepare('SELECT * FROM hotels WHERE id = ?').bind(hotelId).all()
+  return c.json(results[0])
+})
+
+app.put('/api/hotels/:id', async (c) => {
+  const hotelId = c.req.param('id')
+
+  const input = await c.req.json<any>()
+  const query = `UPDATE hotels SET name = "${input.name}", place = "${input.place}", time = ${input.time} WHERE id = "${hotelId}"`
+  const hotel = await c.env.DB.exec(query)
+
+  return c.json(hotel)
+})
+
+app.delete('/api/hotels/:id', async (c) => {
+  const hotelId = c.req.param('id')
+
+  const query = `DELETE FROM hotels WHERE id = "${hotelId}"`
+  const hotel = await c.env.DB.exec(query)
+
+  return c.json(hotel)
+})
+
 export default app
