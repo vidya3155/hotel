33690337c2fc8c359110f34fd0ee9dec08851b2d
vidya3155 <vidymns3155@gmail.com>
2025-06-18 09:25:24 +0700

    tailwind css

--- src/views/CreateHotelView.vue
+++ src/views/CreateHotelView.vue
@@ -27,23 +27,25 @@ const saveData = async () => {
 
 
 <template>
-    <h1>Buat Event Baru</h1>
-    <form @submit.prevent="saveData">
-        <div>
-            <label>Nama</label>
-            <input type="text" v-model="name">
-        </div>
-        <div>
-            <label>Tempat</label>
-            <input type="text" v-model="place">
-        </div>
-        <div>
-            <label>Waktu</label>
-            <input type="datetime-local" v-model="time">
-        </div>
-
-        <div>
-            <input type="submit">
-        </div>
-    </form>
+    <div class="container px-4 py-6 mx-auto">Add commentMore actions
+        <h1 class="text-lg sm:text-2xl">Buat Event Baru</h1>
+        <form @submit.prevent="saveData">
+            <div class="w-full">
+                <label>Nama</label>
+                <input type="text" v-model="name" class="border">
+            </div>
+            <div>
+                <label>Tempat</label>
+                <input type="text" v-model="place" class="border">
+            </div>
+            <div>
+                <label>Waktu</label>
+                <input type="datetime-local" v-model="time" class="border">
+            </div>
+    
+            <div class="mt-3">
+                <input type="submit" class="rounded-full text-white bg-sky-500 py-2 px-6 hover:bg-sky-800 hover:cursor-pointer">
+            </div>
+        </form>
+    </div>
 </template>
